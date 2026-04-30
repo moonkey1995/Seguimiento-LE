008fa2f56b2779be0f0de538a2325ce2d1df68d3
Hector Misael Copca Resendiz <hector.copca@televisaunivision.com>
2026-04-30 10:43:07 -0600

    actu3004

---
+++ correr_dashLE.R
@@ -0,0 +1,38 @@
+
+setwd(here::here())
+
+# 1. Renderizar el archivo .Rmd
+# Esto creará un archivo .html en la misma carpeta
+rmarkdown::render("index.Rmd")
+
+# (Opcional) Mensaje de confirmación en la consola
+print("¡Reporte generado exitosamente!")
+
+library(gert)
+# --- 2. Interactuar con Git ---
+print("Iniciando proceso de Git...")
+
+# (Opcional) Revisar el estado del repositorio
+# Devuelve un data frame con los archivos modificados, nuevos, etc.
+status <- git_status()
+print("Estado del repositorio:")
+print(status)
+
+
+# 3. Añadir los archivos al "staging area" (como 'git add')
+# Vamos a añadir el reporte HTML que acabamos de generar.
+# También podríamos añadir el propio .Rmd si lo hemos modificado.
+print("Añadiendo archivos modificados ('git add')...")
+git_add(".") 
+
+# 4. Hacer el commit (como 'git commit')
+# Creamos un mensaje de commit dinámico con la fecha.
+commit_message <- paste("Reporte actualizado automáticamente el", Sys.Date())
+print(paste("Haciendo 'git commit' con el mensaje:", commit_message))
+git_commit(message = commit_message)
+
+# 5. Subir los cambios al repositorio remoto (como 'git push')
+print("Subiendo los cambios al remoto ('git push')...")
+git_push()
+
+print("¡Proceso de Git finalizado exitosamente!")

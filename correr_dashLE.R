8bba3bd7d970492cad9c4f2c24a0dd22ba040d3d
moonkey1995 <moniqaolivares@gmail.com>
2026-05-04 12:20:26 -0600

    Reporte actualizado automáticamente el 2026-05-04

--- correr_dashLE.R
+++ correr_dashLE.R
@@ -1,9 +1,7 @@
 
-<<<<<<< HEAD
 setwd(here::here())
-=======
+
 setwd("D:/Seguimiento LE/Seguimiento-LE")
->>>>>>> 579eeeff05c5c603ebdff8b1bf65f1223d816b30
 
 # 1. Renderizar el archivo .Rmd
 # Esto creará un archivo .html en la misma carpeta
